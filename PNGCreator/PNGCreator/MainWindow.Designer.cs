22cb8df150e70d3b70cea6e84e3f60081793a7f2
Christer2222 <Christer__11@hotmail.com>
2020-03-18 18:23:49 +0100

    init

---
+++ PNGCreator/PNGCreator/MainWindow.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace PNGCreator
+{
+	partial class BackgroundForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.iPhone1_cb = new System.Windows.Forms.CheckBox();
+			this.iPhone2_cb = new System.Windows.Forms.CheckBox();
+			this.iPad1_cb = new System.Windows.Forms.CheckBox();
+			this.android1_cb = new System.Windows.Forms.CheckBox();
+			this.android2_cb = new System.Windows.Forms.CheckBox();
+			this.createButton = new System.Windows.Forms.Button();
+			this.crop_cb = new System.Windows.Forms.CheckBox();
+			this.iPhone1_p_cb = new System.Windows.Forms.CheckBox();
+			this.iPhone2_p_cb = new System.Windows.Forms.CheckBox();
+			this.iPad1_p_cb = new System.Windows.Forms.CheckBox();
+			this.android1_p_cb = new System.Windows.Forms.CheckBox();
+			this.android3_cb = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// iPhone1_cb
+			// 
+			this.iPhone1_cb.AutoSize = true;
+			this.iPhone1_cb.Location = new System.Drawing.Point(12, 12);
+			this.iPhone1_cb.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+			this.iPhone1_cb.MinimumSize = new System.Drawing.Size(100, 30);
+			this.iPhone1_cb.Name = "iPhone1_cb";
+			this.iPhone1_cb.Size = new System.Drawing.Size(223, 30);
+			this.iPhone1_cb.TabIndex = 0;
+			this.iPhone1_cb.Text = "iPhone 2688 x 1242 pixels (landscape)";
+			this.iPhone1_cb.UseVisualStyleBackColor = true;
+			// 
+			// iPhone2_cb
+			// 
+			this.iPhone2_cb.AutoSize = true;
+			this.iPhone2_cb.Location = new System.Drawing.Point(12, 50);
+			this.iPhone2_cb.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+			this.iPhone2_cb.MinimumSize = new System.Drawing.Size(100, 30);
+			this.iPhone2_cb.Name = "iPhone2_cb";
+			this.iPhone2_cb.Size = new System.Drawing.Size(223, 30);
+			this.iPhone2_cb.TabIndex = 2;
+			this.iPhone2_cb.Text = "iPhone 2436 x 1125 pixels (landscape)";
+			this.iPhone2_cb.UseVisualStyleBackColor = true;
+			// 
+			// iPad1_cb
+			// 
+			this.iPad1_cb.AutoSize = true;
+			this.iPad1_cb.Location = new System.Drawing.Point(12, 88);
+			this.iPad1_cb.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+			this.iPad1_cb.MinimumSize = new System.Drawing.Size(100, 30);
+			this.iPad1_cb.Name = "iPad1_cb";
+			this.iPad1_cb.Size = new System.Drawing.Size(209, 30);
+			this.iPad1_cb.TabIndex = 4;
+			this.iPad1_cb.Text = "iPad 2732 x 2048 pixels (landscape)";
+			this.iPad1_cb.UseVisualStyleBackColor = true;
+			// 
+			// android1_cb
+			// 
+			this.android1_cb.AutoSize = true;
+			this.android1_cb.Location = new System.Drawing.Point(12, 254);
+			this.android1_cb.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+			this.android1_cb.MinimumSize = new System.Drawing.Size(100, 30);
+			this.android1_cb.Name = "android1_cb";
+			this.android1_cb.Size = new System.Drawing.Size(314, 30);
+			this.android1_cb.TabIndex = 6;
+			this.android1_cb.Text = "Android Feature Graphic (1024px by 500px) (No Alpha)";
+			this.android1_cb.UseVisualStyleBackColor = true;
+			// 
+			// android2_cb
+			// 
+			this.android2_cb.AutoSize = true;
+			this.android2_cb.Location = new System.Drawing.Point(12, 330);
+			this.android2_cb.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+			this.android2_cb.MinimumSize = new System.Drawing.Size(100, 30);
+			this.android2_cb.Name = "android2_cb";
+			this.android2_cb.Size = new System.Drawing.Size(217, 30);
+			this.android2_cb.TabIndex = 8;
+			this.android2_cb.Text = "Android Header Image (4096 x 2304)";
+			this.android2_cb.UseVisualStyleBackColor = true;
+			// 
+			// createButton
+			// 
+			this.createButton.Location = new System.Drawing.Point(351, 392);
+			this.createButton.Name = "createButton";
+			this.createButton.Size = new System.Drawing.Size(165, 57);
+			this.createButton.TabIndex = 100;
+			this.createButton.Text = "Create All";
+			this.createButton.UseVisualStyleBackColor = true;
+			this.createButton.Click += new System.EventHandler(this.CreateButton_Click);
+			// 
+			// crop_cb
+			// 
+			this.crop_cb.AutoSize = true;
+			this.crop_cb.Location = new System.Drawing.Point(233, 430);
+			this.crop_cb.Name = "crop_cb";
+			this.crop_cb.Size = new System.Drawing.Size(93, 19);
+			this.crop_cb.TabIndex = 3;
+			this.crop_cb.Text = "Crop image?";
+			this.crop_cb.UseVisualStyleBackColor = true;
+			this.crop_cb.Visible = false;
+			// 
+			// iPhone1_p_cb
+			// 
+			this.iPhone1_p_cb.AutoSize = true;
+			this.iPhone1_p_cb.Location = new System.Drawing.Point(307, 12);
+			this.iPhone1_p_cb.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+			this.iPhone1_p_cb.MinimumSize = new System.Drawing.Size(100, 30);
+			this.iPhone1_p_cb.Name = "iPhone1_p_cb";
+			this.iPhone1_p_cb.Size = new System.Drawing.Size(209, 30);
+			this.iPhone1_p_cb.TabIndex = 1;
+			this.iPhone1_p_cb.Text = "iPhone 1242 x 2688 pixels (portrait)";
+			this.iPhone1_p_cb.UseVisualStyleBackColor = true;
+			// 
+			// iPhone2_p_cb
+			// 
+			this.iPhone2_p_cb.AutoSize = true;
+			this.iPhone2_p_cb.Location = new System.Drawing.Point(307, 50);
+			this.iPhone2_p_cb.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+			this.iPhone2_p_cb.MinimumSize = new System.Drawing.Size(100, 30);
+			this.iPhone2_p_cb.Name = "iPhone2_p_cb";
+			this.iPhone2_p_cb.Size = new System.Drawing.Size(209, 30);
+			this.iPhone2_p_cb.TabIndex = 3;
+			this.iPhone2_p_cb.Text = "iPhone 1125 x 2436 pixels (portrait)";
+			this.iPhone2_p_cb.UseVisualStyleBackColor = true;
+			// 
+			// iPad1_p_cb
+			// 
+			this.iPad1_p_cb.AutoSize = true;
+			this.iPad1_p_cb.Location = new System.Drawing.Point(307, 88);
+			this.iPad1_p_cb.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+			this.iPad1_p_cb.MinimumSize = new System.Drawing.Size(100, 30);
+			this.iPad1_p_cb.Name = "iPad1_p_cb";
+			this.iPad1_p_cb.Size = new System.Drawing.Size(195, 30);
+			this.iPad1_p_cb.TabIndex = 5;
+			this.iPad1_p_cb.Text = "iPad 2048 x 2732 pixels (portrait)";
+			this.iPad1_p_cb.UseVisualStyleBackColor = true;
+			// 
+			// android1_p_cb
+			// 
+			this.android1_p_cb.AutoSize = true;
+			this.android1_p_cb.Location = new System.Drawing.Point(12, 292);
+			this.android1_p_cb.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+			this.android1_p_cb.MinimumSize = new System.Drawing.Size(100, 30);
+			this.android1_p_cb.Name = "android1_p_cb";
+			this.android1_p_cb.Size = new System.Drawing.Size(357, 30);
+			this.android1_p_cb.TabIndex = 7;
+			this.android1_p_cb.Text = "Android Feature Graphic (500px x 1024px) (Portrait) (No Alpha)";
+			this.android1_p_cb.UseVisualStyleBackColor = true;
+			// 
+			// android3_cb
+			// 
+			this.android3_cb.AutoSize = true;
+			this.android3_cb.Location = new System.Drawing.Point(12, 424);
+			this.android3_cb.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+			this.android3_cb.MinimumSize = new System.Drawing.Size(100, 30);
+			this.android3_cb.Name = "android3_cb";
+			this.android3_cb.Size = new System.Drawing.Size(154, 30);
+			this.android3_cb.TabIndex = 9;
+			this.android3_cb.Text = "Android Icon (512 x 512)";
+			this.android3_cb.UseVisualStyleBackColor = true;
+			// 
+			// BackgroundForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(544, 461);
+			this.Controls.Add(this.android3_cb);
+			this.Controls.Add(this.crop_cb);
+			this.Controls.Add(this.createButton);
+			this.Controls.Add(this.android2_cb);
+			this.Controls.Add(this.android1_p_cb);
+			this.Controls.Add(this.android1_cb);
+			this.Controls.Add(this.iPad1_p_cb);
+			this.Controls.Add(this.iPhone2_p_cb);
+			this.Controls.Add(this.iPhone1_p_cb);
+			this.Controls.Add(this.iPad1_cb);
+			this.Controls.Add(this.iPhone2_cb);
+			this.Controls.Add(this.iPhone1_cb);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "BackgroundForm";
+			this.Text = "PNG Maker";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckBox iPhone1_cb;
+		private System.Windows.Forms.CheckBox iPhone2_cb;
+		private System.Windows.Forms.CheckBox iPad1_cb;
+		private System.Windows.Forms.CheckBox android1_cb;
+		private System.Windows.Forms.CheckBox android2_cb;
+		private System.Windows.Forms.Button createButton;
+		private System.Windows.Forms.CheckBox crop_cb;
+		private System.Windows.Forms.CheckBox iPhone1_p_cb;
+		private System.Windows.Forms.CheckBox iPhone2_p_cb;
+		private System.Windows.Forms.CheckBox iPad1_p_cb;
+		private System.Windows.Forms.CheckBox android1_p_cb;
+		private System.Windows.Forms.CheckBox android3_cb;
+	}
+}
+
---
+++ PNGCreator/PNGCreator/MainWindow.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PNGCreator
+{
+	public partial class BackgroundForm : Form
+	{
+		private string fromPath;
+		private string toPath;
+
+		private string appName;
+		private float Percent { 
+			get {
+				try
+				{
+					return ((tasksCompleted / tasks) * 100); 
+				}
+				catch
+				{
+					return 0;
+				}
+			}}
+		private float tasks;
+		private int tasksCompleted;
+
+		public BackgroundForm()
+		{
+			InitializeComponent();
+			string psc = Path.DirectorySeparatorChar.ToString();
+			fromPath = $"{Directory.GetCurrentDirectory()}{psc}From{psc}";
+			toPath = $"{Directory.GetCurrentDirectory()}{psc}To{psc}";
+
+			appName = Text;
+			Text = $"{appName}";
+
+			if (!Directory.Exists(fromPath))
+				Directory.CreateDirectory(fromPath);
+
+			if (!Directory.Exists(toPath))
+				Directory.CreateDirectory(toPath);
+		}
+		
+		private void CreateButton_Click(object sender, EventArgs e)
+		{
+			tasks = tasksCompleted = 0;
+			StartConverts();
+		}
+
+		async void StartConverts()
+		{
+			createButton.Enabled = false;
+			Text = $"{appName} (Working...)";
+
+			List<Task> convertTasks = new List<Task>();
+
+			/*
+			if (iPhone1_cb.Checked) convertTasks.Add( Task.Run(() => ConvertBatch(toPath, 2688, 1424, false, iPhone1_cb.Text)));
+			if (iPhone2_cb.Checked) convertTasks.Add( Task.Run(() => ConvertBatch(toPath, 2436, 1125, false, iPhone2_cb.Text)));
+			if (iPad1_cb.Checked) convertTasks.Add( Task.Run(() => ConvertBatch(toPath, 2732, 2048, false, iPad1_cb.Text)));
+			if (iPad2_cb.Checked) convertTasks.Add( Task.Run(() => ConvertBatch(toPath, 2732, 2048, false, iPad2_cb.Text)));
+			*/
+			if (iPhone1_cb.Checked) convertTasks.Add( ConvertBatch(toPath, 2688, 1424, crop_cb.Checked, iPhone1_cb.Text));
+			if (iPhone2_cb.Checked) convertTasks.Add(ConvertBatch(toPath, 2436, 1125, crop_cb.Checked, iPhone2_cb.Text));
+			if (iPad1_cb.Checked) convertTasks.Add(ConvertBatch(toPath, 2732, 2048, crop_cb.Checked, iPad1_cb.Text));
+
+			if (iPhone1_p_cb.Checked) convertTasks.Add(ConvertBatch(toPath, 1424, 2688, crop_cb.Checked, iPhone1_p_cb.Text));
+			if (iPhone2_p_cb.Checked) convertTasks.Add(ConvertBatch(toPath, 1125, 2436, crop_cb.Checked, iPhone2_p_cb.Text));
+			if (iPad1_p_cb.Checked) convertTasks.Add(ConvertBatch(toPath, 2048, 2732, crop_cb.Checked, iPad1_p_cb.Text));
+
+			if (android1_cb.Checked) convertTasks.Add(ConvertBatch(toPath, 1024, 500, crop_cb.Checked, android1_cb.Text, true));
+			if (android1_p_cb.Checked) convertTasks.Add(ConvertBatch(toPath, 500, 1024, crop_cb.Checked, android1_p_cb.Text, true));
+			if (android2_cb.Checked) convertTasks.Add(ConvertBatch(toPath, 4096, 2304, crop_cb.Checked, android2_cb.Text));
+
+			if (android3_cb.Checked) convertTasks.Add(ConvertBatch(toPath, 512, 512, crop_cb.Checked, android3_cb.Text));
+
+			//foreach (var v in convertTasks) v.Start();
+			//Task.WaitAll(convertTasks.ToArray());
+
+			await Task.Delay(1000);
+		
+			createButton.Enabled = true;
+		}
+
+		async Task ConvertBatch(string path, int targetWidth, int targetHeight, bool crop, string finalName, bool removeAlpha = false)
+		{
+			Bitmap fromImage;
+			Bitmap newImage = new Bitmap(targetWidth, targetHeight);
+
+			int tasksHere = Directory.GetFiles(fromPath).Length;
+			tasks += tasksHere;
+
+			crop = true;
+
+			for (int i = 0; i < tasksHere; i++)
+			{
+				if (crop)
+				{
+					//Method 1, crop
+					using (FileStream fromStream = new FileStream(Directory.GetFiles(fromPath)[i], FileMode.Open))
+					{
+						fromImage = new Bitmap(Image.FromStream(fromStream), new Size(targetWidth, targetHeight));
+					}
+					fromImage.Save($"{toPath}{finalName}_{i}.png", (removeAlpha)? System.Drawing.Imaging.ImageFormat.Jpeg: System.Drawing.Imaging.ImageFormat.Png);
+				}
+				else
+				{
+					//Method 2, scale
+					using (FileStream fromStream = new FileStream(Directory.GetFiles(fromPath)[i], FileMode.Open))
+					{
+						fromImage = new Bitmap(Image.FromStream(fromStream));
+					}
+
+					//with a ratio, a pixel will correspond to one of the ratio distance away
+					float ratioX = (float)fromImage.Width  / targetWidth;
+					float ratioY = (float)fromImage.Height / targetHeight;
+					Color color;
+
+					//set all pixels in _newText
+					for (int x = 0; x < targetWidth; x++)
+					{
+						for (int y = 0; y < targetHeight; y++)
+						{
+							//there are no decimal pixels, so remove the decimals
+							int posX = (int)(x * ratioX);
+							int posY = (int)(y * ratioY);
+
+							color = fromImage.GetPixel(posX, posY);
+							newImage.SetPixel(x, y, color);
+						}
+					}
+					newImage.Save($"{toPath}{finalName}_{i}.png", (removeAlpha) ? System.Drawing.Imaging.ImageFormat.Jpeg : System.Drawing.Imaging.ImageFormat.Png);
+
+					//newImage.Save(toPath + $"{finalName}_{i}.png");
+				}
+
+				await Task.Delay(1);
+				tasksCompleted++;
+				Text = $"{appName} ({Percent.ToString("##0")}%)";
+				await Task.Delay(10);
+			}
+
+			fromImage = null;
+			newImage = null;
+
+			GC.Collect();
+		}
+	}
+}
